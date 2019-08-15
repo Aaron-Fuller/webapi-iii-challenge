e7c0f2c71baf2076205cd252b5e8684fdef1f626
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-08-15 19:44:49 -0400

    Deploying

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+const port = process.env.PORT || 4000;
 const express = require('express');
 const server = express();
 const postRouter = require('./posts/postRouter');
 const userRouter = require('./users/userRouter');
 const serverRouter = require('./server');
+
+
 
 server.use(express.json());
 
@@ -10,6 +14,6 @@ server.use('/', serverRouter)
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 
-const port = 8000;
+const port = 4000;
 
 server.listen(port, () => console.log(`\nAPI RUNNING on port http://localhost:${port} \n`)); 
